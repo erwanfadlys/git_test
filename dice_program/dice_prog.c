e75c26250e55e3f762a4332b7c42db523aa0947a
ErwanFS <erwan.fadlys@gmail.com>
2021-11-23 05:21:06 +0300

    Added dice_program

---
+++ dice_program/dice_prog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int result, rolled;
+    printf("\t\tDice Program\n");
+    printf("================================\n\n");
+    printf("Want to roll the dice?\n\n");
+    char enter = 0;
+    while (enter != '\r' && enter != '\n'){
+        enter = getchar();
+        printf("Enter");
+        }
+        /*stuck at creating enter to continue */
+    printf("================================");
+}
